b523544d4fec5fb39e9643cbfc341b989a6a98b4
Mark <75957685+mbh206@users.noreply.github.com>
2022-09-24 10:53:24 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const scheme = document.getElementById("mode-picker");
+const pickedColor = document.getElementById('picked-color')
+const pickedColorHex = document.getElementById('picked-color-hex')
+let hexValues = []
+pickedColor.addEventListener('change', newColor, false);
+
+function newColor(e){
+    pickedColorHex.textContent = e.target.value;
+}
+
+function render() {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${pickedColor.value.replace('#','')}&mode=${scheme.options[scheme.selectedIndex].value}&count=5`, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            for (let i=1; i < 6; i++){
+                let value = data.colors[i-1].hex.value
+                hexValues.push(value)
+                document.getElementById(`color-${i}`).style.background = value
+                document.getElementById(`color-name-${i}`).innerText = value
+            }
+            })
+}
+render()
